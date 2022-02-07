d52b902115a89b757196a9cb8ae683ba5c199b10
Hovig <hovig.senekjian@hotmail.com>
2022-02-07 22:21:14 +0200

    first commit

---
+++ src/config/pages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'group' => ['type' => "group label", 'label' => "Product Structure"],
+    'categories' => ['repo' =>  "CategoryPage", 'label' => "Categories", 'key' => "categories", 'type' => "link", 'icon' => ""],
+    'subcategories' => ['repo' =>  "SubcategoryPage", 'label' => "Subcategories", 'key' => "subcategories", 'type' => "link", 'icon' => ""],
+    'products' => ['repo' =>  "ProductPage", 'label' => "Products", 'key' => "products", 'type' => "link", 'icon' => ""],
+    'countries' => ['repo' =>  "CountryPage", 'label' => "Countries", 'key' => "countries", 'type' => "link", 'icon' => ""],
+    'users' => ['repo' =>  "UserPage", 'label' => "Users", 'key' => "users", 'type' => "link", 'icon' => ""],
+    'designers' => ['repo' =>  "DesignerPage", 'label' => "Designers", 'key' => "designers", 'type' => "link", 'icon' => ""],
+    'experience' => ['repo' =>  "ExperiencePage", 'label' => "Experience", 'key' => "experience", 'type' => "link", 'icon' => ""],
+
+];
---
+++ src/providers/CMSServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace twa\ecomauth\providers;
+
+use Illuminate\Support\ServiceProvider;
+
+Class CMSServiceProvider extends ServiceProvider{
+    public function boot(){
+        $this->loadViewsFrom(__DIR__.'/../views/', 'CMSViews');
+        $this->publishes([ __DIR__.'/../config/pages.php' => config_path('pages.php') ], 'config');
+    }
+
+    public function register(){
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+    }
+
+
+}
