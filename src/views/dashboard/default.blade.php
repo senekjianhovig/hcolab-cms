7d1507b7d0027a99eeebe5733d844ef272210f94
Hovig <hovig.senekjian@hotmail.com>
2023-04-15 01:04:04 +0300

    fixes

--- src/views/dashboard/default.blade.php
+++ src/views/dashboard/default.blade.php
@@ -22,6 +22,17 @@ h3 {
 <div class="container-fluid mt-4 mb-5">
     <div class="row">
 
+        @if(env('ENABLE_CMS_DASHBOARD_COPYRIGHT') == 1)
+        <div class="col-lg-12 mb-3">
+            <div class="ui segment  px-4 py-5 ">
+                <h2 class="my-0 mb-3">Welcome {{session('admin')->first_name}}!</h2> 
+                <p>
+                    You are currently logged in and using the content management created by HCO. The reproduction of this content management and the use of it for commercial reasons is strictly prohibited, and might result to a direct termination of any maintenance contracts in question or might result in the ownership company taking legal actions.
+                </p>
+            </div>
+        </div>
+        @endif
+
         {!! (new \hcolab\cms\controllers\GadgetsController)->render() !!}
 
     </div>
