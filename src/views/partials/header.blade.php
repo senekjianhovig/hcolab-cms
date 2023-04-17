ac4bd4423af44383bff25bab0f7ed29f6e2924c2
Hovig <hovig.senekjian@hotmail.com>
2023-04-17 09:40:06 +0300

    fixes

--- src/views/partials/header.blade.php
+++ src/views/partials/header.blade.php
@@ -3,46 +3,24 @@
     <div class="d-flex align-items-center">
 
 
-        {{-- <div class="dropdown dropdown-notifications">
-            <a href="#notifications-panel" class="dropdown-toggle" data-toggle="dropdown">
-              <i data-count="0" class="glyphicon glyphicon-bell notification-icon"></i>
-            </a>
-
-            <div class="dropdown-container">
-              <div class="dropdown-toolbar">
-                <div class="dropdown-toolbar-actions">
-                  <a href="#">Mark all as read</a>
-                </div>
-                <h3 class="dropdown-toolbar-title">Notifications (<span class="notif-count">0</span>)</h3>
-              </div>
-              <ul class="dropdown-menu">
-              </ul>
-              <div class="dropdown-footer text-center">
-                <a href="#">View All</a>
-              </div>
-            </div>
-        </div> --}}
-
-        {{-- <div> --}}
-          <div class="ui icon top left pointing notification-popup-trigger " style="margin-right:50px">
-            
+    
+      @if(env('NOTIFICATION_CENTER_ENABLED') && env('NOTIFICATION_CENTER_ENABLED') == 1)
+          <div class="ui icon top left pointing notification-popup-trigger " style="margin-right:50px">  
             <span class="initials">
               <i class="bell icon"></i>
-              {{-- <i class="fa-solid fa-bell-on"></i> --}}
-              {{-- <i class="fa-solid fa-bell-on"></i> --}}
+          
             </span>
             <i class="dropdown icon"></i>
           </div>
-            {{-- <i data-count="0" class="glyphicon glyphicon-bell notification-icon"></i> --}}
-           
+          
           <div class="ui flowing popup hidden notification-popup-panel dropdown-notifications menu">
            
             <div class="ui loading-screen loading" style="height: 100px;">
             
             </div>
               
-            </div>
-        {{-- </div> --}}
+          </div>
+      @endif
 
 
         <div class="welcome">Welcome <br>  {{request()->admin->first_name}} </div>
