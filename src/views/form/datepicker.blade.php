0a14eb3ea549a9ded9ab9b4547a1fcc5eecc8624
Hovig <hovig.senekjian@hotmail.com>
2023-06-21 22:51:34 +0300

    fixes

--- src/views/form/datepicker.blade.php
+++ src/views/form/datepicker.blade.php
@@ -7,10 +7,13 @@
     if($data && property_exists($data,$fieldname) && strtotime($data->$fieldname) == 0){
         $value = "";
     }
+
+    $label_exists = !isset($element->ui->disable_label) || isset($element->ui->disable_label) && $element->ui->disable_label == false;
+    $margin_exists = !isset($element->ui->disable_margin) || isset($element->ui->disable_margin) && $element->ui->disable_margin == false;
 ?>
-<div class="{{$element->ui->container}} mb-4">
+<div class="{{$element->ui->container}} @if($margin_exists) mb-4 @endif">
     <div class="field">
-        <label>{{ $element->ui->label }} @if($element->ui->required) * @endif</label>
+        @if($label_exists) <label>{{ $element->ui->label }} @if($element->ui->required) * @endif</label> @endif
         <input type="date" name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ $value }}"
             @if($element->ui->required)
         required @endif >
